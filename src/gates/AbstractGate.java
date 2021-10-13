303809ee97d23ff2d9d70a1c57f0b8c35d3ea4bf
Einar Kjellback <einarkjellback@fastmail.com>
2021-10-13 12:02:53 +0200

    Implemented AbstractGate.getIn(int)

--- src/gates/AbstractGate.java
+++ src/gates/AbstractGate.java
@@ -5,14 +5,19 @@ import java.util.function.Function;
 
 public abstract class AbstractGate implements Gate {
     private final int outputDim;
+    private final List<Pin> in;
 
-    private AbstractGate(int outputDim) {
+    private AbstractGate(int inputDim, int outputDim) {
         this.outputDim = outputDim;
+        in = new ArrayList<>(inputDim);
+        for (int i = 0; i < inputDim; i++) {
+            in.add(new Pin());
+        }
     }
 
     @Override
     public Pin getIn(int i) {
-        return null;
+        return this.in.get(i);
     }
 
     @Override
@@ -22,14 +27,16 @@ public abstract class AbstractGate implements Gate {
     }
 
     public static Gate fromFunction(int inputDim, int outputDim, Function<List<Boolean>, List<Boolean>> f) {
-        return new AbstractGate(outputDim) {
+        return new AbstractGate(inputDim, outputDim) {
             @Override
             public List<Boolean> input(List<Boolean> input) {
                 if (input.size() != inputDim) {
                     throw new IllegalArgumentException("Expected input size "
                             + inputDim + ", but was " + input.size());
                 }
+
                 List<Boolean> output = f.apply(input);
+
                 if (output.size() != outputDim) {
                     throw new IllegalArgumentException("Expected list of size "
                             + outputDim + " from function " + f + ", but was " + output.size());
